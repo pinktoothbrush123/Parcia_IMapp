5508165957279aba32180fd327c6b3715fb70239
pinktoothbrush13 <jinandreiparcia13@gmail.com>
2021-01-17 16:22:27 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/parcia_imapp/RegisterScreen.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.parcia_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_screen);
+    }
+}
